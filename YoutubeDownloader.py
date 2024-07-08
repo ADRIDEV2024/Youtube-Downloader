d3498e08961e0802239e751d3d9a0736e53996df
Adrian <adriangomis34@gmail.com>
2024-07-08 11:57:26 +0200

    Update YoutubeDownloader.py

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -33,8 +33,9 @@ if __name__ == "__main__":
      
 video_url = input("Please enter a url from a Youtube video: ")
 save_directory = open_file_dialog()
+
  if not save_directory:
        print("Invalid location, please try again")
        
-    else:
+ else:
        download_Video(video_url, save_directory)
